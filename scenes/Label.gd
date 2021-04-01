6b6ff82e9d04c0e6259ba0d2db5111488e379419
kurtosis <kurtosis.g@gmail.com>
2021-04-01 18:04:57 -0400

    Backspace deletes WIP

--- scenes/Label.gd
+++ scenes/Label.gd
@@ -1,8 +1,13 @@
 extends Label
-
+func _ready():
+	self.text = ""
 func _input(event):
 	if event is InputEventKey:
 		if event.pressed and not event.echo:
 			var prompt = OS.get_scancode_string(event.get_scancode_with_modifiers())
-			print(prompt)
-			self.text = prompt
+			if prompt == "BackSpace":
+				print ("Delete")
+				self.text.erase(0, 1)
+			else:
+				print (prompt)
+				self.text += prompt
